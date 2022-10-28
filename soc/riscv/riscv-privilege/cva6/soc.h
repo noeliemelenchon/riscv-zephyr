a63b1a1e913f581d1b35666b5c453ceb98c50e09
Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
2022-10-28 14:37:39 +0200

    Adding CVA6 board definition

---
+++ soc/riscv/riscv-privilege/cva6/soc.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022 Kevin Eyssartier <kevin.eyssartier@thalesgroup.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the CVA6 processor
+ */
+
+#ifndef __RISCV_CVA6_SOC_H_
+#define __RISCV_CVA6_SOC_H_
+
+#include <soc_common.h>
+
+#endif /* __RISCV_CVA6_SOC_H_ */
